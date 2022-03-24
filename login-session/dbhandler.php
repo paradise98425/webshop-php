f96bb43113fdce6526f4f4dc076d90335388f9ad
paradise98425 <paradise98425@gmail.com>
2022-03-24 12:07:56 +0100

    removes the unused file

--- login-session/dbhandler.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbc = mysqli_connect('localhost', 'root', 'password', 'test_php');
-
-if (!$dbc) {
-    echo  "Error: Unable to connect to MySQL" . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-?>
